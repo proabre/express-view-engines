c7e7008753c9391037de372f6e11790a17a1fc77
proabre <proabresh@gmail.com>
2026-09-17 19:53:35 +0300

    route aboute page

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/404", (req, res) => {
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
+});
+
+app.use("/404", (req, res) => {
   res.status(404).render("404");
 });
